c2ff56f51419e4a5ecabf13b1ba67b2fec6a21ab
markdawson <mark@markdawson.xyz>
2017-09-23 18:12:27 -0500

    added dynamic content

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,25 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, "dashboard/index.html")
+
+
+    data = request.GET
+
+    name = data.get('username', '')
+
+    context = {
+        'user_name': name[::-1]
+    }
+
+    return render(request, "dashboard/index.html", context)
+
+
+def user_view(request, name):
+
+    print(name)
+
+    context = {
+        'user_name': name
+    }
+
+    return render(request, "dashboard/index.html", context)
