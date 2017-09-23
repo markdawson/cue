fb8e0afad6386039e6225e4bdca4ef84c9992d25
markdawson <mark@markdawson.xyz>
2017-09-23 18:37:55 -0500

    added verify

--- bot_api/views.py
+++ bot_api/views.py
@@ -1,5 +1,23 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 
 def messages_response(request):
+
+    data = request.GET
+
+
     return JsonResponse({'status': 'success'})
+
+
+def messages_response_verify(request):
+
+    data = request.GET
+
+    token = data.get('hub.verify_token')
+
+    if token == 'cueparty':
+        challenge = data.get('hub.challenge')
+    else:
+        challenge = 'Failure'
+
+    return HttpResponse(challenge)
