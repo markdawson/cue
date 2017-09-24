2e49370c6f04e9d844bf95a1621fee913578d296
markdawson <mark@markdawson.xyz>
2017-09-23 21:42:13 -0500

    adding if statement

--- bot_api/views.py
+++ bot_api/views.py
@@ -20,56 +20,58 @@ def messages_response(request):
 
 
         sender = data['entry'][0]['messaging'][0]['sender']['id']
-        text = data['entry'][0]['messaging'][0]['message']['text']
+        text = data['entry'][0]['messaging'][0]['message'].get('text')
 
-        # payload = {
-        #     "recipient": {
-        #         "id": sender
-        #     },
-        #     "message": {
-        #         "text": text
-        #     }
-        # }
-        #
-        # token = os.environ.get('PAGE_ACCESS_TOKEN')
-        # url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
-        #
-        # r = requests.post(url, json=payload)
-        # logger.info(token)
-        # logger.info(url)
-        # logger.info(r.content)
+        if text:
 
-        token = 'EAAEZBsQCC0fwBACo4UPYZCVd74pQcm2VkorSaDoQJ143SxMUPM8zx6D102UijrhGYTCpZAYRvZBhKdNCen4SO8unytSlAkjM3GygIPwtO1ZBokEcPMsK2ZCubLi0drQbZBDuqyvgfvol5MriB66bPxHaI1dzK7AjqZAkmc5nkZBbrVQZDZD'
-        url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+            # payload = {
+            #     "recipient": {
+            #         "id": sender
+            #     },
+            #     "message": {
+            #         "text": text
+            #     }
+            # }
+            #
+            # token = os.environ.get('PAGE_ACCESS_TOKEN')
+            # url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+            #
+            # r = requests.post(url, json=payload)
+            # logger.info(token)
+            # logger.info(url)
+            # logger.info(r.content)
 
-        payload = {
-            "recipient": {
-                "id": sender
-            },
-            "message": {
-                "text": text
+            token = 'EAAEZBsQCC0fwBACo4UPYZCVd74pQcm2VkorSaDoQJ143SxMUPM8zx6D102UijrhGYTCpZAYRvZBhKdNCen4SO8unytSlAkjM3GygIPwtO1ZBokEcPMsK2ZCubLi0drQbZBDuqyvgfvol5MriB66bPxHaI1dzK7AjqZAkmc5nkZBbrVQZDZD'
+            url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+
+            payload = {
+                "recipient": {
+                    "id": sender
+                },
+                "message": {
+                    "text": text
+                }
             }
-        }
 
-        payload2 = {
-            "recipient": {
-                "id": sender
-            },
-            "message": {
-                "text": 'tehe'
+            payload2 = {
+                "recipient": {
+                    "id": sender
+                },
+                "message": {
+                    "text": 'TeHe'
+                }
             }
-        }
 
 
 
-        r = requests.post(url, json=payload)
-        sleep(1)
-        requests.post(url, json=payload2)
-        logger.info(text)
-        logger.info(sender)
-        logger.info(token)
-        logger.info(url)
-        logger.info(r.content)
+            r = requests.post(url, json=payload)
+            sleep(1)
+            requests.post(url, json=payload2)
+            logger.info(text)
+            logger.info(sender)
+            logger.info(token)
+            logger.info(url)
+            logger.info(r.content)
 
 
         return JsonResponse({'thanks': True})
