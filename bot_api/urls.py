fb77bdffa3cef01cd92b87ac8efd19636947ed96
markdawson <mark@markdawson.xyz>
2017-09-23 22:15:32 -0500

    fixed urls

--- bot_api/urls.py
+++ bot_api/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r"^messages$", views.messages_response, name='messages'),
-    url(r"^messages/verify$", views.messages_response_verify, name='messages'),
 ]
