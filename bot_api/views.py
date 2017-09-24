bba3dcff569e4610d14b34c9c9c997b03bc2441d
markdawson <mark@markdawson.xyz>
2017-09-23 20:35:33 -0500

    hopefully fixed token issue

--- bot_api/views.py
+++ bot_api/views.py
@@ -21,6 +21,26 @@ def messages_response(request):
         sender = data['entry'][0]['messaging'][0]['sender']['id']
         text = data.get('text')
 
+        # payload = {
+        #     "recipient": {
+        #         "id": sender
+        #     },
+        #     "message": {
+        #         "text": text
+        #     }
+        # }
+        #
+        # token = os.environ.get('PAGE_ACCESS_TOKEN')
+        # url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+        #
+        # r = requests.post(url, json=payload)
+        # logger.info(token)
+        # logger.info(url)
+        # logger.info(r.content)
+
+        token = 'EAAEZBsQCC0fwBACo4UPYZCVd74pQcm2VkorSaDoQJ143SxMUPM8zx6D102UijrhGYTCpZAYRvZBhKdNCen4SO8unytSlAkjM3GygIPwtO1ZBokEcPMsK2ZCubLi0drQbZBDuqyvgfvol5MriB66bPxHaI1dzK7AjqZAkmc5nkZBbrVQZDZD'
+        url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+
         payload = {
             "recipient": {
                 "id": sender
@@ -30,13 +50,11 @@ def messages_response(request):
             }
         }
 
-        token = os.environ.get('PAGE_ACCESS_TOKEN')
-        url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
-
         r = requests.post(url, json=payload)
         logger.info(token)
         logger.info(url)
         logger.info(r.content)
+
 
         return JsonResponse({'thanks': True})
 
