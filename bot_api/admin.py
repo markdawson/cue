d6fb141f16978c1aafef524c591024b16955507c
markdawson <mark@markdawson.xyz>
2017-09-24 02:28:20 -0500

    added Place migration

--- bot_api/admin.py
+++ bot_api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import CueUser, Event
+from .models import CueUser, Event, Place
 
 admin.site.register(CueUser)
 admin.site.register(Event)
+admin.site.register(Place)
