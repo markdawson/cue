17f00003839681240c40be94601bbff8ec60e2e6
markdawson <mark@markdawson.xyz>
2017-09-23 19:06:37 -0500

    updated url patterns

--- bot_api/urls.py
+++ bot_api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r"^messages$", views.messages_response, name='messages'),
-    url(r"^messages/verify$", views.messages_response, name='messages'),
+    url(r"^messages/verify$", views.messages_response_verify, name='messages'),
 ]
