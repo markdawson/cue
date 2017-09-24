0b348a278faf6f9b371078825e90dbd086efee9e
markdawson <mark@markdawson.xyz>
2017-09-23 20:13:35 -0500

    added echo

--- bot_api/views.py
+++ bot_api/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import logging
 import json
+import requests
 import os
 
 logger = logging.getLogger('cue.custom')
@@ -14,6 +15,24 @@ def messages_response(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         logger.info(data)
+
+        token = os.environ.get('PAGE_ACCESS_TOKEN')
+
+        sender = data['messaging'][0]['sender']['id']
+        text = data.get('text')
+
+        payload = {
+            "recipient": {
+                "id": sender
+            },
+            "message": {
+                "text": text
+            }
+        }
+
+        url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+
+        requests.post(url, data=payload)
         return JsonResponse({'thanks': True})
 
     else:
