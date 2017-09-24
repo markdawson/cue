0d4f3be9150b9c688158036db1f94ef4a5e4ea68
janei <jiw3@rice.edu>
2017-09-24 02:22:57 -0500

    working on parsing

---
+++ bot_api/parsing.py
@@ -0,0 +1,6 @@
+import re
+
+text = data['entry'][0]['messaging'][0]['message'].get('text')
+
+event_pattern = ".+[cC][uU][eE]\s(.+)\s[aA][tT]\s(.+)\s[aA][tT]\s(.+)$"
+name, time, location = re.findPattern(text, event_pattern)
