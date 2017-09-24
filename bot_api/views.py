5b3f347b3db1face0fdca722ea56ab76f907efdc
markdawson <mark@markdawson.xyz>
2017-09-23 22:13:57 -0500

    added method post to fb

--- bot_api/views.py
+++ bot_api/views.py
@@ -18,64 +18,14 @@ def messages_response(request):
         data = json.loads(request.body)
         logger.info(data)
 
-
-
         sender = data['entry'][0]['messaging'][0]['sender']['id']
         text = data['entry'][0]['messaging'][0]['message'].get('text')
 
         if text:
-
-            # payload = {
-            #     "recipient": {
-            #         "id": sender
-            #     },
-            #     "message": {
-            #         "text": text
-            #     }
-            # }
-            #
-            # token = os.environ.get('PAGE_ACCESS_TOKEN')
-            # url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
-            #
-            # r = requests.post(url, json=payload)
-            # logger.info(token)
-            # logger.info(url)
-            # logger.info(r.content)
-
-            token = 'EAAEZBsQCC0fwBACo4UPYZCVd74pQcm2VkorSaDoQJ143SxMUPM8zx6D102UijrhGYTCpZAYRvZBhKdNCen4SO8unytSlAkjM3GygIPwtO1ZBokEcPMsK2ZCubLi0drQbZBDuqyvgfvol5MriB66bPxHaI1dzK7AjqZAkmc5nkZBbrVQZDZD'
-            url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
-
-            payload = {
-                "recipient": {
-                    "id": sender
-                },
-                "message": {
-                    "text": text
-                }
-            }
-
-            payload2 = {
-                "recipient": {
-                    "id": sender
-                },
-                "message": {
-                    "text": 'teehee'
-                }
-            }
-
-
-
-            r = requests.post(url, json=payload)
-
+            logger.info(post_message_to_fb(sender, text))
             if random.random() < 0.20:
                 sleep(1)
-                requests.post(url, json=payload2)
-            logger.info(text)
-            logger.info(sender)
-            logger.info(token)
-            logger.info(url)
-            logger.info(r.content)
-
+                requests.post(sender, 'teehee')
 
         return JsonResponse({'thanks': True})
 
@@ -93,8 +43,22 @@ def messages_response(request):
     return JsonResponse({'status': 'success'})
 
 
-def messages_response_verify(request):
-    pass
+def post_message_to_fb(to, text):
+
+    token = os.environ.get('PAGE_ACCESS_TOKEN')
+    url = "https://graph.facebook.com/v2.6/me/messages?access_token={}".format(token)
+
+    payload = {
+        "recipient": {
+            "id": to
+        },
+        "message": {
+            "text": text
+        }
+    }
+
+    r = requests.post(url, json=payload)
+    return r
 
 
 
