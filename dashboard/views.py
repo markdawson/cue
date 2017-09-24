fb2bc480bd4d848bede86c07e92023f9db7adaf2
rharpavat <rishuharpavat@gmail.com>
2017-09-23 20:24:05 -0500

    branched!

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, "dashboard/index.html")
+
+    data = request.GET
+
+    name = data['username']
+
+
+    context = {
+        'user_name': name[::-2]
+
+    }
+
+    return render(request, "dashboard/index.html", context)
